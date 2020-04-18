c28a36147865aff70e326b0a335fccd15fff6992
Nico Schäfer <n_schaefer11@cs.uni-kl.de>
2020-04-18 14:05:54 +0200

    Fixed Formatting

--- octoprint_Octoslack/__init__.py
+++ octoprint_Octoslack/__init__.py
@@ -1053,14 +1053,14 @@ class OctoslackPlugin(
                         and analysis_result["analysisPending"]
                     ):
                         return
-		    elif event == "PLUGIN_OCTOLAPSE_MOVIE_DONE":
-				self.handle_event("MovieDone",
-							  	  channel_override,
-							      payload,
-							      override_notification_enabled_check,
-                        	      override_command_enabled_check,
-                                  event_settings_overrides,
-            	)
+            elif event == "PLUGIN_OCTOLAPSE_MOVIE_DONE":
+              self.handle_event("MovieDone",
+                                channel_override,
+                                payload,
+                                override_notification_enabled_check,
+                                override_command_enabled_check,
+                                event_settings_overrides
+              )
 			
             supported_events = self._settings.get(["supported_events"], merged=True)
             if supported_events == None or not event in supported_events:
